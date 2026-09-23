2dafc7525a822ef3b565330949c7c95ab98a1a35
Keerthi-127 <mangalikeerthi7@gmail.com>
2026-09-23 12:10:11 +0530

    Vehicle.java

---
+++ Assignment-2/Vehicle.java
@@ -0,0 +1,10 @@
+public class Vehicle {
+
+    public void start() {
+        System.out.println("Vehicle is starting.");
+    }
+
+    public void stop() {
+        System.out.println("Vehicle is stopping.");
+    }
+}
